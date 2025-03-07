59d66c6367db9e3ffc9b36a7e888bd39c9f5dd56
konovalov_k <konova10v@yandex.ru>
2025-03-07 18:54:00 +0500

    create project

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Кирилл Коновалов on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
